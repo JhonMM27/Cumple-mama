7ee2404f04f2756d2c1987540dcdae37a8db46ae
JhonMM <mesonesjhon27@gmail.com>
2025-06-19 11:55:54 -0500

    Deplay2

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/mami-cumple/",
+  base: "/Cumple-mama/",
 })
